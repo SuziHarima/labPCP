afbcac00f68bcf17740216efe5181774f81f8331
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-08-13 11:40:26 -0300

    adiciona barra de pesquisa

--- src/app/home-admin-docente/home-admin-docente.component.ts
+++ src/app/home-admin-docente/home-admin-docente.component.ts
@@ -4,17 +4,20 @@ import { UserService } from '../shared/services/user.service';
 import { AlunoService } from '../shared/services/aluno.service';
 import { Aluno } from '../shared/interfaces/aluno.interface';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-admin-docente',
   standalone: true,
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, FormsModule],
   templateUrl: './home-admin-docente.component.html',
   styleUrl: './home-admin-docente.component.scss'
 })
 export class HomeAdminDocenteComponent {
   
   listaAlunos: Array<Aluno> = [];
+  textoPesquisa: string | undefined;
+  listaAlunosPesquisa: Array<Aluno> = [];
 
   constructor(private userService: UserService, private alunoService: AlunoService){
     
@@ -23,6 +26,18 @@ export class HomeAdminDocenteComponent {
 
   profile(){
     return this.userService.verifyProfile('admin')
+  }
+
+  pesquisar() {
+    if (this.textoPesquisa) {
+      this.listaAlunosPesquisa = this.listaAlunos.filter((aluno) =>
+        aluno.nome.toUpperCase().includes(this.textoPesquisa!.toUpperCase()) || 
+        aluno.telefone.includes(this.textoPesquisa!.toString()) || 
+        aluno.email.toUpperCase().includes(this.textoPesquisa!.toUpperCase())
+      );
+    } else {
+      this.listaAlunosPesquisa = this.listaAlunos;
+    }
   }
 
 
