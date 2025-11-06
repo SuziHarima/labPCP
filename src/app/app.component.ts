300bf0d7e376b9f83fe9bb9fd67192bf01d3e5b3
suzi <suzi.harima94@gmail.com>
2025-11-06 18:29:52 -0300

    corrige visualização do menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MenuLateralComponent } from "./shared/component/menu-lateral/menu-lateral.component";
 import { ToolBarComponent } from "./shared/component/tool-bar/tool-bar.component";
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuLateralComponent, ToolBarComponent],
+  imports: [RouterOutlet, MenuLateralComponent, ToolBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'labPCP';
+  showMenuLateral = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          // lista de rotas que NÃO devem mostrar o menu lateral
+          const hiddenRoutes = ['/login'];
+          this.showMenuLateral = !hiddenRoutes.includes(event.urlAfterRedirects);
+        }
+      });
+  }
 
 }
